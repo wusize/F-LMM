a0ce4d11d8227f54c44139c1e041573621b42c91
unknown <wusize@softsugar.com>
2024-03-30 13:56:20 +0800

    png

---
+++ frozen_llava/datasets/png.py
@@ -0,0 +1,137 @@
+import os
+import io
+import json
+import copy
+import torch
+import numpy as np
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from PIL import Image
+import random
+try:
+    from petrel_client.client import Client
+except:
+    Client = None
+
+from xtuner.registry import BUILDER
+from mmdet.datasets.api_wrappers.coco_api import COCOPanoptic
+import mmcv
+import io
+from mmengine.fileio import get
+from panopticapi import utils
+
+
+class GCGDataset(Dataset):
+    def __init__(self,
+                 json_file,
+                 panoptic_json_file,
+                 panoptic_png_path,
+                 image_processor=None, tokenizer=None,
+                 ceph_path=None, local_path=None, prompt_template=None):
+        super().__init__()
+        with open(json_file, 'r') as f:
+            self.data = json.load(f)
+        self.coco = COCOPanoptic(panoptic_json_file)
+        self.panoptic_png_path = panoptic_png_path
+        self.ceph_path = ceph_path
+        self.local_path = local_path
+        self.FILE_CLIENT = None
+        self.use_ceph = (Client is not None) and (ceph_path is not None)
+
+        self.tokenizer = BUILDER.build(tokenizer)
+        self.image_processor = BUILDER.build(image_processor)
+        self.prompt = self.tokenizer.encode(
+            prompt_template['INSTRUCTION'].format(input='<image>\nWhat is shown in this image?'),
+            add_special_tokens=True)
+        self.prompt_template = prompt_template
+
+    @staticmethod
+    def _load_segm(segm_path):
+        img_bytes = get(segm_path)
+        pan_png = mmcv.imfrombytes(
+            img_bytes, flag='color', channel_order='rgb').squeeze()
+        segm_map = utils.rgb2id(pan_png)
+
+        return segm_map
+
+    def __len__(self):
+        return len(self.data)
+
+    def read_image(self, image_file):
+        if self.use_ceph:
+            image_path = os.path.join(self.ceph_path, image_file)
+            if self.FILE_CLIENT is None:
+                self.FILE_CLIENT = Client()
+            img_bytes = self.FILE_CLIENT.get(image_path)
+            image = Image.open(io.BytesIO(img_bytes))
+        else:
+            image_path = os.path.join(self.local_path, image_file)
+            image = Image.open(image_path)
+
+        return image
+
+    def __getitem__(self, index):
+        data_sample = self.data[index]
+        mask_cnt = 0
+        caption_input_ids = []
+        mask_ids = []
+        mask_segment_ids = []
+        for segment in data_sample['segments']:
+            segment_input_ids = self.tokenizer.encode(segment['utterance'], add_special_tokens=False)
+            caption_input_ids += segment_input_ids
+            if len(segment['segment_ids']) == 0:
+                mask_ids += [-1] * len(segment_input_ids)
+            else:
+                mask_ids += [mask_cnt] * len(segment_input_ids)
+                mask_segment_ids.append(segment['segment_ids'])
+                # todo: load masks
+                mask_cnt += 1
+
+        if mask_cnt == 0:
+            return self.__getitem__(random.choice(range(self.__len__())))
+
+        image_id = int(data_sample['image_id'])
+        image_info = self.coco.imgs[image_id]
+        segm_file = image_info['segm_file']
+        segm_map = self._load_segm(os.path.join(self.panoptic_png_path, segm_file))
+
+        masks = []
+
+        for mask_segment_ids_ in mask_segment_ids:
+            mask = 0
+            for segment_id in mask_segment_ids_:
+                mask += (segm_map == segment_id).astype(np.uint8)
+            masks.append(np.clip(mask, a_max=1, a_min=0))
+        assert len(masks) == mask_cnt
+
+        input_ids = self.prompt + caption_input_ids
+        input_ids = torch.tensor(input_ids, dtype=torch.long)
+        mask_ids = torch.tensor(mask_ids)
+
+        image = self.read_image(image_info['file_name'])
+        image_data = self.image_processor.preprocess(image)
+
+        pixel_values = torch.from_numpy(image_data['pixel_values'][0])
+        meta_data = image_data['meta_datas'][0]
+
+        masks = torch.from_numpy(np.stack(masks))
+        resized_masks = F.interpolate(masks[None], size=pixel_values.shape[2:])[0]
+
+        h, w = meta_data['image_shape']['height'], meta_data['image_shape']['width']
+        masks = F.interpolate(masks[None], size=(h, w))[0]
+
+        p_h, p_w = meta_data['padded_shape']['height'], meta_data['padded_shape']['width']
+
+        padded_masks = torch.zeros(mask_cnt, p_h, p_w, dtype=masks.dtype)
+        padding = meta_data['padding']
+
+        padded_masks[:, padding['before_height']:p_h-padding['after_height'],
+                        padding['before_width']:p_w-padding['after_width']] = masks
+
+        return dict(input_ids=input_ids,
+                    mask_ids=mask_ids,
+                    pixel_values=pixel_values,
+                    resized_masks=resized_masks,   # shape is not kept
+                    padded_masks=padded_masks,
+                    masks=masks,   # shape is kept
+                    image_sizes=torch.tensor(image_data['image_sizes'][0]))
