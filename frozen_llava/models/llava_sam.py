12bfa706eccb219ca6ab8008910b27ec6bb8fb72
unknown <wusize@softsugar.com>
2024-04-12 03:35:49 +0800

    points

--- frozen_llava/models/llava_sam.py
+++ frozen_llava/models/llava_sam.py
@@ -1,72 +1,27 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-from mmengine.model import BaseModel
-from xtuner.registry import BUILDER
+from frozen_llava.models.meta_arch import FrozenLlava, compute_mask_IoU
+from frozen_llava.models.segment_modules.sam_wrapper import SAMWrapper
 
 
-@torch.no_grad()
-def compute_mask_IoU(masks, target):
-    temp = masks * target
-    intersection = temp.sum(dim=-1)
-    union = ((masks + target) - temp).sum(dim=-1)
-    return intersection / (union + 1e-12)
-
-
-class FrozenLlava(BaseModel):
-
+class FrozenLlavaSAM(FrozenLlava):
     def __init__(self,
-                 model,
-                 mask_head,
-                 merge='mean',
-                 loss_mask=None,
-                 loss_dice=None):
-        super().__init__()
-        self.llava = BUILDER.build(model)
-        self.llava.requires_grad_(False)
-        in_channels = (self.llava.config.text_config.num_attention_heads *
-                       self.llava.config.text_config.num_hidden_layers*2)
-        mask_head.update(
-            in_channels=in_channels)
-        self.mask_head = BUILDER.build(mask_head)
-        self.patch_size = self.llava.config.vision_config.patch_size
-        self.merge = merge
-        assert merge in ['mean', 'max']
+                 sam_name, sam_checkpoint,
+                 sam_weight=1.0,
+                 intermediate_weight=0.1,
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sam = SAMWrapper(model_name=sam_name, checkpoint=sam_checkpoint)
+        self.text_proj = nn.Linear(self.llava.config.vision_config.hidden_size,
+                                   self.sam.model.prompt_encoder.embed_dim)
+        self.text_layer_weights = nn.Parameter(
+            torch.ones(self.llava.config.text_config.num_hidden_layers))
+        self.sam_weight = sam_weight
+        self.intermediate_weight = intermediate_weight
 
-        self.loss_mask = BUILDER.build(loss_mask)
-        self.loss_dice = BUILDER.build(loss_dice)
-
-    def apply_merge(self, x, dim=1):
-        if self.merge == 'mean':
-            return x.mean(dim=dim)
-        elif self.merge == 'max':
-            return x.max(dim=dim).values
-        else:
-            raise NotImplementedError
-
-    def init_weights(self):
-        pass
-
-    def train(self, mode=True):
-        self.llava.train(mode=False)
-        self.mask_head.train(mode=mode)
-        self.training = mode
-        return self
-
-    def forward(self, data, data_samples=None, mode='loss'):
-        if mode == 'loss':
-            return self.compute_loss(data)
-        elif mode == 'predict':
-            return self.predict(data)
-        elif mode == 'tensor':
-            return self._forward(data)
-        else:
-            raise NotImplementedError
-
-    def _forward(self, data):
-        return None
-
-    def predict(self, data):
-        return None
+    def get_text_layer_weights(self):
+        return torch.softmax(self.text_layer_weights, dim=0)
 
     def compute_loss(self, data):
         mask_cnts = 0
@@ -74,6 +29,11 @@ class FrozenLlava(BaseModel):
         loss_mask = 0
         accuracy = 0
         aiou = 0
+        sam_loss_dice = 0
+        sam_loss_mask = 0
+        sam_accuracy = 0
+        sam_aiou = 0
+        text_layer_weights = self.get_text_layer_weights()
         for data_sample in data:
             assert data_sample['pixel_values'].shape[0] > 1
             inputs = dict(input_ids=data_sample['input_ids'][None].to(self.llava.device),
@@ -85,11 +45,14 @@ class FrozenLlava(BaseModel):
                                         dtype=torch.bool)
             with torch.no_grad():
                 outputs = self.llava(**inputs,
-                                     attention_mask=attention_mask, output_attentions=True)
+                                     attention_mask=attention_mask,
+                                     output_hidden_states=True,
+                                     output_attentions=True)
             fine_image_feature_h, fine_image_feature_w = outputs['image_feature_shapes'][0]
             mask_ids = outputs['mask_ids']
             attentions = [attn[0, ..., outputs['image_to_overwrite'][0]]
                           for attn in outputs.attentions]
+            hidden_states = outputs.hidden_states
             del outputs
 
             coarse_image_h, coarse_image_w = data_sample['pixel_values'].shape[2:]
@@ -109,6 +72,7 @@ class FrozenLlava(BaseModel):
 
             attentions_with_coarse_list = []
             attentions_with_fine_list = []
+            text_embeds = []
             for mask_id in range(len(masks)):
                 matched = mask_ids[0] == mask_id
                 assert matched.sum() > 0
@@ -119,6 +83,13 @@ class FrozenLlava(BaseModel):
                     [self.apply_merge(attn[:, matched], dim=1) for attn in attentions_with_fine])
                 attentions_with_coarse_list.append(mask_attentions_with_coarse)
                 attentions_with_fine_list.append(mask_attentions_with_fine)
+
+                # num_layers, matched_seq_len, hidden_size
+                matched_hidden_states = torch.stack([hs[0, matched] for hs in hidden_states])
+                matched_hidden_states *= text_layer_weights.view(-1, 1, 1)
+                # matched_seq_len, hidden_size
+                text_embeds.append(self.text_proj(matched_hidden_states.sum(0)))
+
             # print('==================debug================', flush=True)
             attentions_with_coarse = torch.stack(attentions_with_coarse_list)
             attentions_with_fine = torch.stack(attentions_with_fine_list)
@@ -137,33 +108,54 @@ class FrozenLlava(BaseModel):
             gt_masks = F.interpolate(masks.to(attention_maps)[None].float(),
                                      size=(fine_image_feature_h, fine_image_feature_w))[0].to(self.llava.dtype)
             assert pred_masks.shape == gt_masks.shape
+            import pdb; pdb.set_trace()
+            sam_pred_masks = self.sam(data_sample['image'], pred_masks, text_embeds)
+            sam_gt_masks = F.interpolate(masks.to(attention_maps)[None].float(),
+                                         size=sam_pred_masks.shape[-2:])[0].to(self.llava.dtype)
             mask_cnts += mask_cnt
 
-            # dice loss
-            loss_dice += self.loss_dice(
-                pred_masks.view(mask_cnt, -1), gt_masks.view(mask_cnt, -1),
-                avg_factor=mask_cnt) * mask_cnt
+            loss_dice_, loss_mask_, accuracy_, aiou_ = self._compute(pred_masks, gt_masks)
+            loss_dice += loss_dice_ * mask_cnt
+            loss_mask += loss_mask_ * mask_cnt
+            accuracy += accuracy_ * mask_cnt
+            aiou += aiou_ * mask_cnt
 
-            weight = torch.ones_like(pred_masks.view(-1))
-            num_pos = gt_masks.sum()
-            num_neg = mask_cnt*fine_image_feature_h*fine_image_feature_w - num_pos
-            weight[gt_masks.view(-1) > 0] /= (num_pos + 1e-12)
-            weight[gt_masks.view(-1) < 1] /= (num_neg + 1e-12)
-            # mask loss
-            loss_mask += self.loss_mask(
-                pred_masks.view(-1),
-                gt_masks.view(-1),
-                weight=weight,
-                avg_factor=2) * mask_cnt
-            acc = torch.eq((pred_masks.detach().sigmoid() > 0.5).to(gt_masks),
-                           gt_masks).to(gt_masks).mean()
-            accuracy += acc * mask_cnt
-            aiou += compute_mask_IoU((pred_masks.detach().sigmoid() > 0.5).to(gt_masks).view(mask_cnt, -1),
-                                     gt_masks.view(mask_cnt, -1)).mean() * mask_cnt
+            sam_loss_dice_, sam_loss_mask_, sam_accuracy_, sam_aiou_ = self._compute(sam_pred_masks, sam_gt_masks)
+            sam_loss_dice += sam_loss_dice_ * mask_cnt
+            sam_loss_mask += sam_loss_mask_ * mask_cnt
+            sam_accuracy += sam_accuracy_ * mask_cnt
+            sam_aiou += sam_aiou_ * mask_cnt
 
         assert mask_cnts > 0
         loss_dict = {'loss_mask': loss_mask / mask_cnts,
                      'loss_dice': loss_dice / mask_cnts,
                      'accuracy': accuracy / mask_cnts,
-                     'aiou': aiou / mask_cnts}
+                     'aiou': aiou / mask_cnts,
+                     'sam_loss_mask': sam_loss_mask / mask_cnts,
+                     'sam_loss_dice': sam_loss_dice / mask_cnts,
+                     'sam_accuracy': sam_accuracy / mask_cnts,
+                     'sam_aiou': sam_aiou / mask_cnts,
+                     }
+        for k, v in loss_dict.items():
+            if 'sam_loss' in k:
+                loss_dict[k] *= self.sam_weight
+            elif 'loss' in k:
+                loss_dict[k] *= self.intermediate_weight
+
         return loss_dict
+
+    def _compute(self, pred_masks, gt_masks):
+        mask_cnt = pred_masks.shape[0]
+        loss_dice = self.loss_dice(
+            pred_masks.view(mask_cnt, -1), gt_masks.view(mask_cnt, -1),
+            avg_factor=mask_cnt)
+        loss_mask = self.loss_mask(
+            pred_masks.view(-1),
+            gt_masks.view(-1),
+            avg_factor=pred_masks.numel())
+        accuracy = torch.eq((pred_masks.detach().sigmoid() > 0.5).to(gt_masks),
+                            gt_masks).to(gt_masks).mean()
+        aiou = compute_mask_IoU((pred_masks.detach().sigmoid() > 0.5).to(gt_masks).view(mask_cnt, -1),
+                                gt_masks.view(mask_cnt, -1)).mean()
+
+        return loss_dice, loss_mask, accuracy, aiou
---
+++ frozen_llava/models/segment_modules/sam_wrapper.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from segment_anything import sam_model_registry, predictor, SamAutomaticMaskGenerator
+from segment_anything.utils.transforms import ResizeLongestSide
+
+
+def mask2box(mask, original_image_size):
+    h, w = mask.shape
+    original_h, original_w = original_image_size
+    ys, xs = np.where(mask)
+    y0, y1 = ys.min(), ys.max()
+    x0, x1 = xs.min(), xs.max()
+
+    return np.array([x0, y0, x1, y1]) * np.array([original_w/w, original_h/h] * 2)
+
+
+class SAMWrapper(nn.Module):
+    def __init__(self, model_name, checkpoint):
+        super(SAMWrapper, self).__init__()
+        self.model = sam_model_registry[model_name](checkpoint=checkpoint)
+        self.model.image_encoder.requires_grad_(False)
+        self.transform = ResizeLongestSide(self.model.image_encoder.img_size)
+
+    def train(self, mode=True):
+        super().train(mode=mode)
+        self.model.image_encoder.eval()
+        self.training = mode
+        return self
+
+    @torch.no_grad()
+    def encode_image(self, image):
+        image = np.array(image.convert(self.model.image_format))
+        input_image = self.transform.apply_image(image)
+        input_image_torch = torch.as_tensor(input_image, device=self.model.device)
+        transformed_image = input_image_torch.permute(2, 0, 1).contiguous()[None, :, :, :]
+
+        original_image_size = image.shape[:2]
+        input_size = transformed_image.shape[-2:]
+
+        features = self.model.image_encoder(self.model.preprocess(transformed_image))
+
+        return features, original_image_size, input_size
+
+    def forward(self, image, pred_masks, text_embeds):
+        # masks are in logits
+        image_embedding, original_image_size, input_size = self.encode_image(image)
+        image_embedding.requires_grad = True
+        prompt_masks = F.interpolate(pred_masks[:, None].float(), size=(256, 256), mode='bilinear').to(pred_masks)
+
+        sam_masks = []
+        for prompt_mask, pred_mask, text_embed in zip(prompt_masks, pred_masks, text_embeds):
+            box = mask2box(pred_mask.cpu().numpy(), original_image_size)
+            box = self.transform.apply_boxes(box, original_image_size)
+            box_torch = torch.as_tensor(box, dtype=torch.float, device=self.device)
+            box_torch = box_torch[None, :]
+            sparse_embeddings, dense_embeddings = self.model.prompt_encoder(
+                points=None,
+                boxes=box_torch,
+                masks=prompt_mask.view(1, 1, 256, 256),
+            )
+            sparse_embeddings = torch.cat([sparse_embeddings, text_embed[None]], dim=1)
+            low_res_masks, iou_predictions = self.model.mask_decoder(
+                image_embeddings=image_embedding,
+                image_pe=self.model.prompt_encoder.get_dense_pe(),
+                sparse_prompt_embeddings=sparse_embeddings,
+                dense_prompt_embeddings=dense_embeddings,
+                multimask_output=False,
+            )
+
+            sam_mask = self.model.postprocess_masks(low_res_masks, input_size, original_image_size)
+            sam_masks.append(sam_mask[0, 0])
+            
+        return torch.stack(sam_masks)
