bd6a8e6b0e1bf3f7a4f80c0a754f32d9b5cc5bf3
wusize <wusize99@gmail.com>
2024-08-04 00:07:42 +0800

    rebuttal

--- scripts/reason_seg.py
+++ scripts/reason_seg.py
@@ -22,9 +22,9 @@ def intersectionAndUnionGPU(output, target, K, ignore_index=255):
     target = target.view(-1)
     output[target == ignore_index] = ignore_index
     intersection = output[output == target]
-    area_intersection = intersection.sum()  # torch.histc(intersection, bins=K, min=0, max=K - 1)
-    area_output = output.sum()  # torch.histc(output, bins=K, min=0, max=K - 1)
-    area_target = target.sum()  # torch.histc(target, bins=K, min=0, max=K - 1)
+    area_intersection = torch.histc(intersection, bins=K, min=0, max=K - 1)
+    area_output = torch.histc(output, bins=K, min=0, max=K - 1)
+    area_target = torch.histc(target, bins=K, min=0, max=K - 1)
     area_union = area_output + area_target - area_intersection
     return area_intersection, area_union, area_target
 
